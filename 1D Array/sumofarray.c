cbd3597cd61ae03ba2b8a9161a782502664a0823
varunbansal644 <varunbansal644@gmail.com>
2019-11-10 17:59:59 +0530

    Sum of Array

---
+++ 1D Array/sumofarray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int i,sum=0;
+  int arr[10];
+  for(i=0;i<=9;i++)
+  {
+    scanf("%d",&arr[i]);
+  }
+  for(i=0;i<=9;i++)
+  {
+    sum=sum+arr[i];
+  }
+  printf("%d",sum);
+    return 0;
+}
