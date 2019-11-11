734f81a899faf9027a21afa9c6332197d00a2539
anshu1515 <57590337+anshu1515@users.noreply.github.com>
2019-11-11 12:31:38 +0530

    Add files via upload

---
+++ 1D Array/insert_num_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+int size,num,pos,i;
+scanf("%d %d %d",&size,&num,&pos);
+int arr[size];
+for(i=0;i<size;i++)
+{
+    scanf("%d",&arr[i]);
+}
+for(i=size-1;i>=pos;i--)
+{
+    arr[i]=arr[i-1];
+}
+arr[pos-1]=num;
+printf("the elements of array:");
+for(i=0;i<size;i++)
+{
+    printf("%d ",arr[i]);
+}
+
+    return 0;
+}
